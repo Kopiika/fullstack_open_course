f09044dec7ec1e014b0e4e011df46fc257ea1e6e
Eleonora <eleonora.kopiika@gmail.com>
2025-11-23 13:15:43 +0200

    Added ex. 3.19 - 2.21 3.19: Added validation for names: minimum 3 characters, only letters and spaces; frontend now displays error messages when a creation fails due to validation. 3.20: Added custom validation for phone numbers: minimum 8 characters, format XX-XXXXXXX or XXX-XXXXXXX; server returns an error for invalid numbers. 3.21: Deploying the database backend to production – Created production build of the frontend and added it to the backend; verified locally and deployed to Render.

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -9,13 +9,38 @@ mongoose.connect(process.env.MONGODB_URI)
 	 console.error('Error connecting to MongoDB:', error.message)
   })
 
-  // схема 
+  // Schema 
   const personSchema = new mongoose.Schema({
-	name: String,
-	number: String
+	name: {
+		type:String,
+		minlength: 3,
+		required: [true, 'Person name required'],
+		validate: {
+      validator: function(v) {
+        return /^[A-Za-z\s]+$/.test(v) // only letters and spaces allowed
+      },
+      message: props => `${props.value} contains invalid characters. Only letters and spaces are allowed!`
+    }
+	},
+	number: {
+		type:String,
+		required: [true, 'Person phone number required'],
+		validate: {
+			validator: function(v) {
+				if (v.length < 8) return false 
+				return /^\d{2,3}-\d+$/.test(v) // number format 046-2035591
+			},
+			message: props => {
+				if (props.value.length < 8){
+					return 'Phone number must be at least 8 characters long'
+				}
+				return `${props.value} is not a valid phone number! Right format is XX-XXXXXXX or XXX-XXXXXXX`
+			}
+		}
+	}
 	})
 
-	// Кастомний toJSON 
+	// Custom toJSON 
 	personSchema.set('toJSON', {
 	transform: (document, returnedObject) => {
 	  returnedObject.id = returnedObject._id.toString()
