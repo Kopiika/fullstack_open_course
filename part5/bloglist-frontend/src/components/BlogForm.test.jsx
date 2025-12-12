d87e8d01be84bde3e2f167a42d8756c98378f438
Eleonora <eleonora.kopiika@gmail.com>
2025-12-12 12:54:15 +0200

    added ex. 5.14-5.16 (Installed a library user-event)

---
+++ part5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import BlogForm from './BlogForm'
+import userEvent from '@testing-library/user-event'
+
+test('<BlogForm /> calls createBlog with correct details when new blog is created', async () => {
+  const createBlog = vi.fn()
+  const user = userEvent.setup()
+
+  render(<BlogForm createBlog={createBlog} />)
+
+  /*const input = screen.getAllByRole('textbox')*/
+  const inputTitle = screen.getByPlaceholderText('title')
+  const inputAuthor = screen.getByPlaceholderText('author')
+  const inputUrl = screen.getByPlaceholderText('url')
+  const submitButton = screen.getByText('Create')
+
+
+  await user.type(inputTitle, 'Testing a form')
+  await user.type(inputAuthor, 'Test Author')
+  await user.type(inputUrl, 'https://test-form.com')
+  await user.click(submitButton)
+  console.log(createBlog.mock.calls)
+
+  expect(createBlog).toHaveBeenCalledTimes(1)
+  expect(createBlog).toHaveBeenCalledWith({
+	 title: 'Testing a form',
+    author: 'Test Author',
+    url: 'https://test-form.com'
+  })
+})
