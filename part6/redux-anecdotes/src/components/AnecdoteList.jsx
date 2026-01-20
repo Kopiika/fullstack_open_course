5ad477dca0564fa467a534ffd170c5bd748fe07b
Eleonora <eleonora.kopiika@gmail.com>
2026-01-20 12:48:02 +0200

    added ex.6.9 Anecdotes, step 7

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -15,11 +15,16 @@ const Anecdote = ({ anecdote, handleClick }) => {
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => [...state].sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
+
+  const filteredAnecdotes = anecdotes
+    .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+    .sort((a, b) => b.votes - a.votes)
 
   return (
     <div>
-      {anecdotes.map(anecdote => (
+      {filteredAnecdotes.map(anecdote => (
         <Anecdote
           key={anecdote.id}
           anecdote={anecdote}
---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,19 @@
+const initialState = ''
+
+const filterReducer = (state = initialState, action) => {
+	switch (action.type) {
+	  case 'SET_FILTER':
+		 return action.payload
+	  default:
+		 return state
+	}
+ }
+ 
+ export const filterChange = (value) => {
+	return {
+	  type: 'SET_FILTER',
+	  payload: value
+	}
+ }
+ 
+ export default filterReducer
