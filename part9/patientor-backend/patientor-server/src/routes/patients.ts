43256e48e7cc62a6e9b0d6e34d747c703ad03cd5
Eleonora <eleonora.kopiika@gmail.com>
2026-03-08 16:43:32 +0200

    added Validating requests

--- part9/patientor-backend/patientor-server/src/routes/patients.ts
+++ part9/patientor-backend/patientor-server/src/routes/patients.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 import patientsService from "../services/patientsService";
+import toNewPatient from "../utils";
 
 const patientsRouter = express.Router();
 
@@ -10,15 +10,17 @@ patientsRouter.get("/", (_req, res) => {
 });
 
 patientsRouter.post("/", (req, res) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const newPatient = patientsService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation,
-  });
-  res.json(newPatient);
+	try { 
+		const newPatient = toNewPatient(req.body);
+		const addedPatient = patientsService.addPatient(newPatient);
+		res.json(addedPatient);
+	} catch (error: unknown) {
+		let errorMessage = "Something went wrong.";
+		if (error instanceof Error) {
+			errorMessage += " Error: " + error.message;
+		}
+		res.status(400).send(errorMessage);
+	}
 });
 
 export default patientsRouter;
---
+++ part9/patientor-backend/patientor-server/src/utils.ts
@@ -0,0 +1,69 @@
+import { NewPatient, Gender } from "./types";
+
+const isString = (text: unknown): text is string => {
+	return typeof text === 'string' || text instanceof String;
+};
+
+const isGender = (param: string): param is Gender => {
+	return Object.values(Gender).map(g => g.toString()).includes(param);
+};
+
+const parseName = (name: unknown): string => {
+	if (!name || !isString(name)) {
+		throw new Error("Incorrect or missing name");
+	}
+	return name;
+};
+
+const parseDateOfBirth = (date: unknown): string => {
+  if (!date || !isString(date)) {
+    throw new Error("Incorrect or missing dateOfBirth");
+  }
+  return date;
+};
+
+const parseSsn = (ssn: unknown): string => {
+  if (!ssn || !isString(ssn)) {
+    throw new Error("Incorrect or missing ssn");
+  }
+  return ssn;
+};
+
+const parseOccupation = (occupation: unknown): string => {
+  if (!occupation || !isString(occupation)) {
+    throw new Error("Incorrect or missing occupation");
+  }
+  return occupation;
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isString(gender) || !isGender(gender)) {
+    throw new Error("Incorrect or missing gender");
+  }
+  return gender;
+};
+
+const toNewPatient = (object: unknown): NewPatient => {
+	if (!object || typeof object !== "object") {
+    throw new Error("Incorrect or missing data");
+  }
+	 if (
+     "name" in object &&
+     "dateOfBirth" in object &&
+     "ssn" in object &&
+     "gender" in object &&
+     "occupation" in object
+   ) {
+     const newPatient: NewPatient = {
+       name: parseName(object.name),
+       dateOfBirth: parseDateOfBirth(object.dateOfBirth),
+       ssn: parseSsn(object.ssn),
+       gender: parseGender(object.gender),
+       occupation: parseOccupation(object.occupation),
+     };
+     return newPatient;
+	};
+	throw new Error("Incorrect data: some fields are missing");
+};
+
+export default toNewPatient;
